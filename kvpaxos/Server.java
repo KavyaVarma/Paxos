71f7945cc540fe270bdab2f224dd5e863f049cc1
Mehnaz Yunus <mehnaz138@gmail.com>
2022-11-06 10:28:34 -0600

    add timeout for status check

--- kvpaxos/Server.java
+++ kvpaxos/Server.java
@@ -1,4 +1,5 @@
 package kvpaxos;
+
 import paxos.Paxos;
 import paxos.State;
 // You are allowed to call Paxos.Status to check if agreement was made.
@@ -30,7 +31,7 @@ public class Server implements KVPaxosRMI {
     Integer logSequence;
 
     // TODO: Duplicate detection (piggyback??)
-    public Server(String[] servers, int[] ports, int me){
+    public Server(String[] servers, int[] ports, int me) {
         this.me = me;
         this.servers = servers;
         this.ports = ports;
@@ -40,28 +41,43 @@ public class Server implements KVPaxosRMI {
         this.kvStore = new HashMap<>();
         this.logSequence = 0;
 
-
-        try{
+        try {
             System.setProperty("java.rmi.server.hostname", this.servers[this.me]);
             registry = LocateRegistry.getRegistry(this.ports[this.me]);
             stub = (KVPaxosRMI) UnicastRemoteObject.exportObject(this, this.ports[this.me]);
             registry.rebind("KVPaxos", stub);
-        } catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    private Op waitTillDecided(int logSequence) {
+        int timeout = 10;
+        while (true) {
+            retStatus ret = this.px.Status(logSequence);
+            if (ret.state == State.Decided)
+                return (Op) ret.v;
+            try {
+                Thread.sleep(timeout);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (timeout < 1000) {
+                timeout = timeout * 2;
+            }
+        }
+    }
 
     // RMI handlers
     // TODO: timeout to wait for consensus to be arrived at??
-    public Response Get(Request request){
+    public Response Get(Request request) {
         // Your code here
 
-        for(; logSequence<=px.Max(); logSequence++) {
+        for (; logSequence <= px.Max(); logSequence++) {
             retStatus ret = px.Status(logSequence);
 
-            Op updateOp = (Op)ret.v;
-            if(updateOp.getOp().equalsIgnoreCase(PUT)) {
+            Op updateOp = (Op) ret.v;
+            if (updateOp.getOp().equalsIgnoreCase(PUT)) {
                 this.kvStore.put(updateOp.getKey(), updateOp.getValue());
             }
             px.Done(logSequence);
@@ -69,18 +85,16 @@ public class Server implements KVPaxosRMI {
 
         Boolean updated = false;
         Op op = new Op(GET, logSequence, request.getKey(), null);
-        while(!updated) {
+        while (!updated) {
             px.Start(logSequence, op);
-            retStatus ret = px.Status(logSequence);
+            Op decidedOp = this.waitTillDecided(logSequence);
 
-            if(!this.isOpEqual((Op)ret.v, op)) {
-                Op updateOp = (Op)ret.v;
-                if(updateOp.getOp().equalsIgnoreCase(PUT)) {
-                    this.kvStore.put(updateOp.getKey(), updateOp.getValue());
+            if (!this.isOpEqual(decidedOp, op)) {
+                if (decidedOp.getOp().equalsIgnoreCase(PUT)) {
+                    this.kvStore.put(decidedOp.getKey(), decidedOp.getValue());
                 }
                 px.Done(logSequence);
-            }
-            else {
+            } else {
                 updated = true;
             }
             px.Done(logSequence);
@@ -91,32 +105,30 @@ public class Server implements KVPaxosRMI {
     }
 
     // TODO: timeout to wait for consensus to be arrived at??
-    public Response Put(Request request){
+    public Response Put(Request request) {
         // Your code here
-        for(; logSequence<=px.Max(); logSequence++) {
+        for (; logSequence <= px.Max(); logSequence++) {
             retStatus ret = px.Status(logSequence);
 
-            Op updateOp = (Op)ret.v;
-            if(updateOp.getOp().equalsIgnoreCase(PUT)) {
+            Op updateOp = (Op) ret.v;
+            if (updateOp.getOp().equalsIgnoreCase(PUT)) {
                 this.kvStore.put(updateOp.getKey(), updateOp.getValue());
             }
             px.Done(logSequence);
         }
-        
+
         Boolean updated = false;
         Op op = new Op(PUT, logSequence, request.getKey(), request.getValue());
-        while(!updated) {
+        while (!updated) {
             px.Start(logSequence, op);
-            retStatus ret = px.Status(logSequence);
+            Op decidedOp = this.waitTillDecided(logSequence);
 
-            if(!this.isOpEqual((Op)ret.v, op)) {
-                Op updateOp = (Op)ret.v;
-                if(updateOp.getOp().equalsIgnoreCase(PUT)) {
-                    this.kvStore.put(updateOp.getKey(), updateOp.getValue());
+            if (!this.isOpEqual(decidedOp, op)) {
+                if (decidedOp.getOp().equalsIgnoreCase(PUT)) {
+                    this.kvStore.put(decidedOp.getKey(), decidedOp.getValue());
                 }
                 px.Done(logSequence);
-            }
-            else {
+            } else {
                 this.kvStore.put(op.getKey(), op.getValue());
                 updated = true;
             }
@@ -124,13 +136,12 @@ public class Server implements KVPaxosRMI {
             logSequence += 1;
         }
         return new Response(true);
-
     }
 
     private boolean isOpEqual(Op op1, Op op2) {
-        return op1.getOp().equalsIgnoreCase(op2.getOp()) 
-            && op1.getClientSeq()==op2.getClientSeq ()
-            && op1.getKey().equalsIgnoreCase(op2.getKey())
-            && op1.getValue() == op2.getValue();
+        return op1.getOp().equalsIgnoreCase(op2.getOp())
+                && op1.getClientSeq() == op2.getClientSeq()
+                && op1.getKey().equalsIgnoreCase(op2.getKey())
+                && op1.getValue() == op2.getValue();
     }
 }
